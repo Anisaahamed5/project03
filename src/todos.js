5211f1f755fdfb5b7e37cf02a6ea446bf9ad6bb9
Anisa Ahamed <anisa@Sams-MBP-2.lan>
2020-10-15 23:05:12 -0700

    Added "Add task" bar

--- src/todos.js
+++ src/todos.js
@@ -1,5 +1,31 @@
 import React from "react";
+import "./todos.css"
 
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            task: ""
+            
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="form-group">
+                    <label for="task">Task</label>
+                    <input type="text" className="form-control" id="tasks" onChange={this.firstname_task} value={this.state.task}/>
+                    
+                    <span class="input-group-addon">
+                        <button type="button" className="btn btn-primary">Add</button>
+                    </span>
+
+            </div>
+        )
+    }
+}
 
 class Todos extends React.Component {
     constructor(props) {
@@ -13,6 +39,8 @@ class Todos extends React.Component {
     render() {
 
         return <div>
+            <AddTodo></AddTodo>
+            
 
         </div>
 
