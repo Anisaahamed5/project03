fa5d05f175dd0fa7d32cb6c382a1ce321f635525
Anisa Ahamed <anisa@Sams-MBP-2.lan>
2020-10-15 22:25:32 -0700

    Added navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import {
   Link
 } from "react-router-dom";
 
+import Todos from "./todos.js";
+import Contact from "./contact.js";
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>
+      {/* NAVBAR */}
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Todos<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Contact</a>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
         <Switch>
-          <Route path="/">
-            <Todos />
-          </Route>
           <Route path="/contact">
             <Contact />
+          </Route>
+          <Route path="/">
+            <Todos />
           </Route>
         </Switch>
       </div>
--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,47 @@
 import React from "react";
+import "./contact.css";
 
 class Contact extends React.Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstname: "",
+            lastname: "",
+            email: "",
+            comments: "",
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className="form-group">
+                    <label for="firstname">First Name</label>
+                    <input type="text" className="form-control" id="firstname"/>
+                </div>
+
+                <div className="form-group">
+                    <label for="lastname">Last Name</label>
+                    <input type="text" className="form-control" id="lastname"/>
+                </div>
+
+                <div className="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" className="form-control" id="email"/>
+                </div>
+
+                <div className="form-group">
+                    <label for="comments">Comments</label>
+                    <input type="textarea" className="form-control" id="comments"/>
+                </div>
+
+
+
+            </>
+        );
+    }
+
 }
+
+export default Contact;
